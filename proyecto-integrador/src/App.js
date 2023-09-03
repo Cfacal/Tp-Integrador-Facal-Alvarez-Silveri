ae55234f6503ad39a8ddd7099e90a596b4e0b237
pedroa912 <palvarezbrescia@udesa.edu.ar>
2023-09-03 13:35:44 -0300

    not found

--- proyecto-integrador/src/App.js
+++ proyecto-integrador/src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import Home from './components/Home/Home';
 import DetalleCanciones from './components/DetalleCanciones/DetalleCanciones';
 import DetalleAlbum from './components/DetalleAlbum/DetalleAlbum';
+import NoEncontrada from './components/NoEncontrada/NoEncontrada';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
             <Route path="/" exact={true} component={Home}/>
             <Route path="/canciones/:id" component = {DetalleCanciones}/>
             <Route path="/albumes/:id" component = {DetalleAlbum}/>
+            <Route path="" component = {NoEncontrada}/>
         </Switch>
         </>
   );
---
+++ proyecto-integrador/src/components/NoEncontrada/NoEncontrada.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NoEncontrada() {
+    return (
+        <React.Fragment>
+          <h1>Error 404 no se encontró esta página</h1>
+        </React.Fragment>
+      );
+    };
+
+    export default NoEncontrada;
