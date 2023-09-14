48b23ea0adcdcfb4e879cc1092071142209640fe
MateoSilveri <msilveri@udesa.edu.ar>
2023-09-14 18:49:33 -0300

    favoritos screen no funciona todavia

--- proyecto-integrador/src/screens/Favoritos/Favoritos.js
+++ proyecto-integrador/src/screens/Favoritos/Favoritos.js
@@ -1,3 +1,58 @@
+import React, { Component } from "react";
+import Albums from "../../components/Albums/Albums";
+
+import Contenedor from "../../components/Contenedor/Contenedor";
+
+let recuperoStorage = localStorage.getItem('favoritos')
 
 
 
+console.log(recuperoStorage);
+
+let favoritosAlbum = JSON.parse(recuperoStorage);
+
+
+class Favoritos extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            favoritosAlbum : []
+        }
+      }
+    
+      componentDidMount() {
+        
+        fetch('https://thingproxy.freeboard.io/fetch/https://api.deezer.com/chart/0/albums?index=0&limit=5')
+          .then((res) => res.json())
+          .then((data) =>
+            this.setState({
+              albumes: data,
+            })
+          )
+          .catch((err) => console.log(err));
+
+      }
+
+    render(){
+        return (
+            <React.Fragment>
+    
+            <h1>Favoritos</h1>
+            <section id="favoritosPelis" ></section>
+             {this.state.favoritosAlbum.map((album, idx) => {
+            if (idx === favoritosAlbum.length) {
+              return (<Albums
+              albumes={album}
+            />)
+            } else {return (null)}
+          })} 
+            
+            {this.state.favoritosAlbum.length > 0 ? <Contenedor array={this.state.favoritosAlbum}/> : <h3>Cargando...</h3>}
+
+            </React.Fragment>
+        );    
+    }
+}
+
+
+export default Favoritos
