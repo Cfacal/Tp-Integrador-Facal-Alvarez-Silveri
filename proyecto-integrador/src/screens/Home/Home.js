244e40a4dd829262483498dbd7eeac972a081309
MateoSilveri <msilveri@udesa.edu.ar>
2023-09-15 23:04:42 -0300

    cambios finales

--- proyecto-integrador/src/screens/Home/Home.js
+++ proyecto-integrador/src/screens/Home/Home.js
@@ -3,6 +3,7 @@ import Footer from "../../components/Footer/Footer";
 import Contenedor from "../../components/Contenedor/Contenedor";
 import Formulario from "../../components/Formulario/Formulario";
 import Navbar from "../../components/Navbar/Navbar";
+import { Link } from "react-router-dom";
 
 class Home extends Component {
     constructor(){
@@ -39,8 +40,11 @@ render(){
         <h1>Tendencias</h1>
         <h2>Canciones más escuchadas</h2>
         <Contenedor info = {this.state.canciones} esAlbum={false}/>
+        <Link className="linkHomeVT"to="/VerTodasTracks">Ver todos los Tracks</Link>
         <h2>Albumes más escuchados</h2>
         <Contenedor info = {this.state.albumes} esAlbum={true}/>
+        <Link className="linkHomeVT"to="/VerTodasAlbum">Ver todos los Albums</Link>
+
         <Footer/>
         </>
     )
